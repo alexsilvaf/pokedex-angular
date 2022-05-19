5474d28f3a48db71aaae6f4a70fd077ab6199c01
sala19a <pedroh.mix@gmail.com>
2022-05-19 19:28:49 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {RouterModule} from "@angular/router";
-import {AppRoutingModule} from "./app-routing.module";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
-import {MatCardModule} from "@angular/material/card";
-import { LikeComponent } from './like/like.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    PokemonComponent,
-    LikeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    MatToolbarModule,
-    RouterModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
