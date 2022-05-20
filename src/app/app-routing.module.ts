60d9b6d4df0984a254adb0cc48fb614e4f258f3c
sala19a <alex120999@hotmail.com.br>
2022-05-19 21:35:49 -0300

    Criando homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {LikeComponent} from "./like/like.component";
 
 const routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent, children: [
+      {path: 'like', component: LikeComponent}
+    ]
+  }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import {MatCardModule} from "@angular/material/card";
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
+    PokemonComponent,
+    LikeComponent,
   ],
-    imports: [
-        BrowserModule,
-        MatToolbarModule,
-        RouterModule,
-        AppRoutingModule,
-        BrowserAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    RouterModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
